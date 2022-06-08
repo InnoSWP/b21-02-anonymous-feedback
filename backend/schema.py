481da989e8d931cbe24e7496bf9a0df119bb2d15
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-08 18:28:36 +0000

    chore: prepare boilerplate

--- backend/schema.py
+++ backend/schema.py
@@ -8,41 +8,40 @@ import strawberry
 @strawberry.type
 class Query:
     @strawberry.field
-    def session(self, id: strawberry.ID) -> 'Session':
+    def session(self, id: strawberry.ID) -> "Session":
         pass
 
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def create_session(self, name: str) -> 'Session':
+    def create_session(self, name: str) -> "Session":
         pass
 
 
 @strawberry.type
 class Subscription:
     @strawberry.subscription
-    async def watch_session(self, id: strawberry.ID) -> 'Message':
+    async def watch_session(self, id: strawberry.ID) -> "Message":
         pass
 
-
-# Sessions
 
 @strawberry.type
 class Session:
     id: strawberry.ID
     name: str
-    created: 'Timestamp'
-    messages: typing.List['Message']
+    created: "Timestamp"
+    messages: typing.List["Message"]
 
 
 @strawberry.type
 class Message:
     message: str
-    timestamp: 'Timestamp'
+    timestamp: "Timestamp"
 
 
 # Utility types
+
 
 @strawberry.type
 class Timestamp:
