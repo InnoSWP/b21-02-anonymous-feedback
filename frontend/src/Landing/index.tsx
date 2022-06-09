61d9949283b604a7b37ea193831676dbf71d8051
SnejUgal <contact@snejugal.ru>
2022-06-09 10:42:40 +0300

    Prepare landing content container

--- frontend/src/Landing/index.tsx
+++ frontend/src/Landing/index.tsx
@@ -1,7 +1,14 @@
+import Wrapper from "../Wrapper";
 import "./style.scss";
 
 const Landing = () => {
-  return <div className="landing"></div>;
+  return (
+    <div className="landing">
+      <Wrapper>
+        <main className="landing_content"></main>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Landing;
