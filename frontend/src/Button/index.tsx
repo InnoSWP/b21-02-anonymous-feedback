3dec686a05617f998fb727e2b1c9eaac13f45ea6
SnejUgal <contact@snejugal.ru>
2022-06-10 11:55:09 +0300

    Redirect to session creation when clicking the button

--- frontend/src/Button/index.tsx
+++ frontend/src/Button/index.tsx
@@ -1,25 +1,38 @@
 import "./style.scss";
 
 import classNames from "classnames";
+import { Link } from "react-router-dom";
 
-interface Props {
+interface PropsBase {
   size?: `big` | `small`;
   color?: `main` | `iu` | `red`;
-  type?: `button` | `submit`;
-  tag?: `a` | `button`;
-  isDisabled?: boolean;
   children: string;
 }
 
-const Button = ({
-  size = `small`,
-  color = `main`,
-  type = `button`,
-  isDisabled = false,
-  children,
-}: Props) => {
+interface ButtonProps extends PropsBase {
+  isDisabled?: boolean;
+  type?: `button` | `submit`;
+}
+
+interface LinkProps extends PropsBase {
+  to: string;
+}
+
+type Props = ButtonProps | LinkProps;
+
+const Button = (props: Props) => {
+  const { size = `small`, color = `main`, children } = props;
   const className = classNames(`button`, `-${size}`, `-${color}`);
 
+  if (`to` in props) {
+    return (
+      <Link to={props.to} className={className}>
+        {children}
+      </Link>
+    );
+  }
+
+  const { type, isDisabled = false } = props;
   return (
     <button type={type} className={className} disabled={isDisabled}>
       {children}
