b399b3d3c46d9483aa2e6440af34f2dce4636c94
SnejUgal <contact@snejugal.ru>
2022-06-28 10:52:31 +0300

    Monitor WebSocket connection status

--- frontend/src/client/webSocket.tsx
+++ frontend/src/client/webSocket.tsx
@@ -1,4 +1,5 @@
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import EventEmitter from "events";
 import { createClient } from "graphql-ws";
 
 const makeWebSocketUrl = (pathname: string) => {
@@ -17,3 +18,38 @@ const webSocketClient = createClient({
   shouldRetry: () => true,
 });
 export const webSocketLink = new GraphQLWsLink(webSocketClient);
+
+export type ConnectionStatus =
+  | `disconnected`
+  | `connecting`
+  | `connected`
+  | `error`;
+
+class ConnectionWatcher extends EventEmitter {
+  status: ConnectionStatus = `disconnected`;
+
+  set(newStatus: ConnectionStatus) {
+    this.status = newStatus;
+    this.emit(`change`, newStatus);
+  }
+
+  constructor() {
+    super();
+    webSocketClient.on(`closed`, () => this.set(`disconnected`));
+    webSocketClient.on(`connected`, () => this.set(`connected`));
+    webSocketClient.on(`connecting`, () => this.set(`connecting`));
+    webSocketClient.on(`error`, () => this.set(`error`));
+  }
+
+  on(_: `change`, handler: (status: ConnectionStatus) => void): this {
+    super.on(`change`, handler);
+    return this;
+  }
+
+  off(_: `change`, handler: (status: ConnectionStatus) => void): this {
+    super.off(`change`, handler);
+    return this;
+  }
+}
+
+export const connectionStatus = new ConnectionWatcher();
